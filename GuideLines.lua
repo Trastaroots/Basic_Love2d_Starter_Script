16e7099f43374f4924ae4fdb7007ec7957d8b334
Trastaroots <56780836+Trastaroots@users.noreply.github.com>
2020-04-29 18:29:58 +0200

    Update GuideLines.lua

--- GuideLines.lua
+++ GuideLines.lua
@@ -3,11 +3,11 @@ guideLines = false
 function GuideLines()
   
   if guideLines == true then
-    love.graphics.setColor(0.5,0.5,0.5,0.25)
+    love.graphics.setColor(unpack(colors.default_B))
     love.graphics.line(sOriginX,0,sOriginX,screenHeight)
     love.graphics.line(0,sOriginY,screenWidth,sOriginY)
   
-    love.graphics.setColor(0,1,0)
+    love.graphics.setColor(unpack(colors.green_A))
     love.graphics.setPointSize(5)
     love.graphics.points(sOriginX,sOriginY)
   end
