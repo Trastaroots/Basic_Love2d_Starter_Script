7fa91e58ef9a9fc3cba11fe0df11c88ed21a73a1
Trastaroots <56780836+Trastaroots@users.noreply.github.com>
2020-04-29 18:30:51 +0200

    Update DebugPanel.lua

--- DebugPanel.lua
+++ DebugPanel.lua
@@ -2,9 +2,9 @@
   
   function DebugPanel()
     if debugPanel == true then
-      love.graphics.setColor(0,0,0,0.75)
+      love.graphics.setColor(unpack(colors.black_A))
       love.graphics.rectangle("fill",0,0,205,140)  
-      love.graphics.setColor(0,1,0)
+      love.graphics.setColor(unpack(colors.green_A))
       love.graphics.print("Current FPS: "..tostring(currentFps),5,5)
       love.graphics.print("Current Time: "..tostring(currentTime),5,20)
       love.graphics.print("Resolution: "..tostring(screenWidth)..(" X ")..tostring(screenHeight)..(" px"),5,35)
@@ -20,10 +20,10 @@
         love.graphics.print("-- Press G to hide guideLines --",5,125)    
       end
     else
-      love.graphics.setColor(0,0,0,0.75)
+      love.graphics.setColor(unpack(colors.black_A))
       love.graphics.rectangle("fill",0,0,215,35)
       
-      love.graphics.setColor(0,1,0)    
+      love.graphics.setColor(unpack(colors.green_A))    
       love.graphics.print("-- Press Tab to open debugPanel --",5,5)
       
       if guideLines == false then
